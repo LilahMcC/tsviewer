5b4e14bc8acee2765ed70c668c1489af63249374
Max Czapanskiy <maxczap@stanford.edu>
2022-02-25 09:31:19 -0800

    plot layout

--- scratch/shiny.R
+++ scratch/shiny.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+library(patchwork)
 library(shiny)
 
 ui <- fillPage(
@@ -6,6 +8,7 @@ ui <- fillPage(
 
 server <- function(input, output, session) {
   ts_data <- dummyts
+  ts_data$y2 <- dummyts$y * 100
 
   i1 <- 1
   i2 <- nrow(ts_data)
@@ -14,9 +17,18 @@ server <- function(input, output, session) {
 
   max_res <- 1e3
   i_decimated <- floor(seq(i1, i2, length.out = min(i2 - i1 + 1, max_res)))
+
   zoom_data <- ts_data[i_decimated, ]
 
-  output$tsplot <- renderPlot(plot(y ~ t, zoom_data, type = "l"))
+  p1 <- ggplot(zoom_data, aes(t, y)) +
+    geom_line() +
+    theme_minimal()
+  p2 <- ggplot(zoom_data, aes(t, y2)) +
+    geom_line() +
+    theme_minimal()
+  p <- p1 | p2
+
+  output$tsplot <- renderPlot(p)
 }
 
 shinyApp(ui, server)
