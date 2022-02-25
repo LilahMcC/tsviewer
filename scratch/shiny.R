ed3ae73899b73bd3af3b1de0f3d446505e3502a5
Max Czapanskiy <maxczap@stanford.edu>
2022-02-25 10:04:54 -0800

    link slider to plots

--- scratch/shiny.R
+++ scratch/shiny.R
@@ -25,30 +25,40 @@ ui <- fillPage(
 )
 
 server <- function(input, output, session) {
-  i1 <- 1
-  i2 <- nrow(ts_data)
-  t1 <- ts_data$t[i1]
-  t2 <- ts_data$t[i2]
-
   max_res <- 1e3
-  i_decimated <- floor(seq(i1, i2, length.out = min(i2 - i1 + 1, max_res)))
-  i_zoomed <- floor(seq(i1, i2, length.out = min(i2 - i1 + 1, max_res)))
-
+  i_decimated <- floor(
+    seq(1, nrow(ts_data), length.out = min(nrow(ts_data), max_res))
+  )
   ts_decimated <- ts_data[i_decimated, ]
-  ts_zoomed <- ts_data[i_decimated, ]
 
-  p1 <- ggplot(ts_decimated, aes(t, y)) +
-    geom_line() +
-    theme_minimal()
-  p2 <- ggplot(ts_zoomed, aes(t, y)) +
-    geom_line() +
-    theme_minimal()
-  p3 <- ggplot(ts_zoomed, aes(t, y2)) +
-    geom_line() +
-    theme_minimal()
-  p <- p1 / p2 / p3 + plot_layout(heights = c(2, 6, 2))
+  output$tsplot <- renderPlot({
+    date_range <- input$tsslider
 
-  output$tsplot <- renderPlot(p)
+    i1 <- which.min(abs(ts_data$t - date_range[1]))
+    i2 <- which.min(abs(ts_data$t - date_range[2]))
+
+    i_zoomed <- floor(seq(i1, i2, length.out = min(i2 - i1 + 1, max_res)))
+    ts_zoomed <- ts_data[i_zoomed, ]
+
+    p1 <- ggplot(ts_decimated, aes(t, y)) +
+      geom_line() +
+      annotate("rect",
+               xmin = ts_data$t[i1],
+               xmax = ts_data$t[i2],
+               ymin = -Inf,
+               ymax = Inf,
+               fill = "black",
+               color = NA,
+               alpha = 0.25) +
+      theme_minimal()
+    p2 <- ggplot(ts_zoomed, aes(t, y)) +
+      geom_line() +
+      theme_minimal()
+    p3 <- ggplot(ts_zoomed, aes(t, y2)) +
+      geom_line() +
+      theme_minimal()
+    p1 / p2 / p3 + plot_layout(heights = c(2, 6, 2))
+  })
 }
 
 shinyApp(ui, server)
