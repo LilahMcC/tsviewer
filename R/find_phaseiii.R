65d8a3ca43f750c5adb2042ee3ec39ee912ca7e9
Max Czapanskiy <maxczap@stanford.edu>
2022-06-21 14:42:22 -0700

    Interface with Google Drive/Sheets to access Phase III data

---
+++ R/find_phaseiii.R
@@ -0,0 +1,30 @@
+#' Find Phase III deployment workbook sheets id
+#'
+#' @param deployid Deployment id e.g. "bw180905-53"
+#'
+#' @return A `sheets_id` object
+find_phaseiii <- function(deployid) {
+  phaseiii_drid <- "1SjiVVzHdOiLK5UnwQt0RA184_TN4-XNE"
+  phaseiii_dr <- googledrive::drive_get(id = phaseiii_drid)
+  gsid <- googledrive::drive_ls(phaseiii_dr, pattern = deployid)[["id"]]
+  if (length(gsid) == 0) {
+    stop("No Phase III workbooks found for deployid ", deployid)
+  } else if (length(gsid) == 1) {
+    googlesheets4::as_sheets_id(gsid)
+  } else {
+    stop("Multiple Phase III workbooks found for deployid ", deployid)
+  }
+}
+
+#' Read a Phase III deployment workbook
+#'
+#' @param deployid
+#'
+#' @return A `tibble` with columns `deployid`, `motionlessid`,
+#' `motionlessstart`, `motionlessend`, and `duration_s`
+read_phaseiii <- function(deployid) {
+  gsid <- find_phaseiii(deployid)
+  googlesheets4::with_gs4_quiet(
+    googlesheets4::read_sheet(gsid)
+  )
+}
